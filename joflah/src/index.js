edd298928d7e237790d048427044d6f8127bf0e3
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-08-25 15:25:13 +0530

    Updates

---
+++ joflah/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Make sure the path is correct
+
+ReactDOM.render(<App />, document.getElementById('root'));
