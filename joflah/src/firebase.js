1c40abf322e3c1f779f6fd8a379f6325edf57a54
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-08-29 08:34:48 +0530

    updates

---
+++ joflah/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1nv0ttW89ko3YuM8g53adM1IOs3zALB8",
+  authDomain: "joflah-chat.firebaseapp.com",
+  projectId: "joflah-chat",
+  storageBucket: "joflah-chat.appspot.com",
+  messagingSenderId: "972270017964",
+  appId: "1:972270017964:web:0a5169d09ea2f7f526ae09"
+};
+export const app = initializeApp(firebaseConfig);
+export const auth=getAuth();
--- joflah/src/pages/Register.jsx
+++ joflah/src/pages/Register.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Add from "../img/addAvatar.png"
 const Register = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const displayName=e.target[0].value;
+        const email=e.target[1].value;
+        const password=e.target[2].value;
+        const file=e.target[3].files[0];
+    }
+
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Joflah</span>
                 <br />
                 <span className="title">Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="username" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
