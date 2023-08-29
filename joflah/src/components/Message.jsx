6de618cb617ff3e8fe32648aeb30a0f5d69b945b
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-08-29 08:09:09 +0530

    Updates

--- joflah/src/components/Message.jsx
+++ joflah/src/components/Message.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className="message">
-      <div className="messageInfo"></div>
-      <div className='messageContent'></div>
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60" alt="" />
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60" alt="" />
+      </div>
     </div>
   )
 }
